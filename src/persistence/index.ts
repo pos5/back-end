90d8b3eb056001116db790b89d2d572c02ce84d1
Victor G <victorgordian103@gmail.com>
2019-10-06 22:31:12 -0700

    go

--- src/persistence/index.ts
+++ src/persistence/index.ts
@@ -2,8 +2,8 @@ import { createConnection, Connection } from "typeorm";
 
 export const openConnection: any = async () =>
   await createConnection({
-    type: "sqlite",
-    database: "database.sqlite",
+    type: "postgres",
+    database: "database.postgres",
     entities: [__dirname + "/../entity/*.ts"],
     migrations: [__dirname + "/../persistence/migration/*.ts"],
     logging: true,
